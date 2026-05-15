0d3dd8fba0c946668e319dd8d5e102582bdc431b
Mansour <mansour.06.ab.06@gmail.com>
2026-05-15 20:31:36 +0300

    Creating a Project class structure

---
+++ src/main/java/com/apps/PortfolioApp/Model/Entity/Project.java
@@ -0,0 +1,67 @@
+package com.apps.PortfolioApp.Model.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "Project")
+@Entity
+public class Project {
+    //  here I will Create Class variables :
+    private int id ;
+    private String name;
+    private String description;
+    private String techStack;
+    private String githubLink;
+
+    // here I will Create a constructor :
+    public Project() {}
+
+    public Project(int id, String name, String description, String techStack, String githubLink) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.techStack = techStack;
+        this.githubLink = githubLink;
+    }
+
+    // Getter && Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    public String getGithubLink() {
+        return githubLink;
+    }
+
+    public void setGithubLink(String githubLink) {
+        this.githubLink = githubLink;
+    }
+}

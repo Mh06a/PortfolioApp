f57bbc3cdebb4d5ce1a52de339df2840dc162b8c
Mansour <mansour.06.ab.06@gmail.com>
2026-05-15 20:11:46 +0300

    Creating a user class structure

---
+++ src/main/java/com/apps/PortfolioApp/Model/Entity/User.java
@@ -0,0 +1,48 @@
+package com.apps.PortfolioApp.Model.Entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "User")
+@Entity
+public class User {
+    // here I will Create Class variables :
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id ;
+    private String username;
+    private String password;
+
+    // here I will Create a constructor :
+    public User() {}
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    // getter && setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

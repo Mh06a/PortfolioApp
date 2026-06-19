8a8f5e3b1985b7d3d5bab67b2b019504bfc4b7a0
Mansour <mansour.06.ab.06@gmail.com>
2026-06-20 01:43:08 +0300

    Add DuplicateSkillException , DuplicateProjectException

--- src/main/java/com/apps/PortfolioApp/Service/ProjectService.java
+++ src/main/java/com/apps/PortfolioApp/Service/ProjectService.java
@@ -14,46 +14,42 @@ public class ProjectService {
     // step1 ----> Create an object from ProjectRepository class :
     private final ProjectRepository projectRepository;
 
-
     // step2 ----> Create a con :
     public ProjectService(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository ;
+        this.projectRepository = projectRepository;
     }
-
 
     // step3 ----> here I will Create a function to Insert to db :
     public ProjectDTO insertProject(ProjectDTO projectDTO) {
+        if (projectRepository.findByName(projectDTO.getName()).isPresent()) {
+            throw new DuplicateProjectException(
+                    "Project already exists with name: " + projectDTO.getName());
+        }
         Project project = ProjectDTO.fromDTOToEntity(projectDTO);
         return ProjectDTO.fromEntityToDTO(projectRepository.save(project));
     }
-
 
     // step4 ----> here I will Create a function to get all projects from db :
     public List<ProjectDTO> getAllProjects() {
         if (projectRepository.findAll().isEmpty()) {
             throw new ProjectNotFoundException("You don't have any project");
-        }
-        else {
+        } else {
             List<Project> projects = projectRepository.findAll();
             return projects.stream().map(ProjectDTO::fromEntityToDTO).toList();
         }
     }
 
-
     // step5 ----> here I will Create a function to get a project by id :
     public ProjectDTO getProjectByID(int id) {
         Project project = projectRepository.findById(id).orElseThrow(
-                () -> new ProjectNotFoundException("Project not found with id: " + id)
-        );
+                () -> new ProjectNotFoundException("Project not found with id: " + id));
         return ProjectDTO.fromEntityToDTO(project);
     }
 
-
     // step6 ----> here I will Create a function update a project from db :
-    public ProjectDTO updateProject(String name ,ProjectDTO projectDTO) {
+    public ProjectDTO updateProject(String name, ProjectDTO projectDTO) {
         Project project = projectRepository.findByName(name).orElseThrow(
-                () -> new ProjectNotFoundException("Project not found with name: " + name)
-        );
+                () -> new ProjectNotFoundException("Project not found with name: " + name));
         project.setName(projectDTO.getName());
         project.setDescription(projectDTO.getDescription());
         project.setTechStack(projectDTO.getTechStack());
@@ -61,21 +57,17 @@ public class ProjectService {
         return ProjectDTO.fromEntityToDTO(projectRepository.save(project));
     }
 
-
     // step7 ----> here I will Create a function delete a project from db :
     public void deleteProject(int id) {
         Project project = projectRepository.findById(id).orElseThrow(
-                () -> new ProjectNotFoundException("Project not found with id: " + id)
-        );
+                () -> new ProjectNotFoundException("Project not found with id: " + id));
         projectRepository.delete(project);
     }
-
 
     // step8 ----> here I will Create a function to find by Skill name :
     public ProjectDTO getProjectByName(String name) {
         Project project = projectRepository.findByName(name).orElseThrow(
-                () -> new ProjectNotFoundException("Project not found with project name: " + name)
-        );
+                () -> new ProjectNotFoundException("Project not found with project name: " + name));
         return ProjectDTO.fromEntityToDTO(project);
     }
 
--- src/main/java/com/apps/PortfolioApp/Service/SkillService.java
+++ src/main/java/com/apps/PortfolioApp/Service/SkillService.java
@@ -1,4 +1,5 @@
 package com.apps.PortfolioApp.Service;
+
 import com.apps.PortfolioApp.DTO.SkillDTO;
 import com.apps.PortfolioApp.ExceptionHandler.DuplicateSkillException;
 import com.apps.PortfolioApp.ExceptionHandler.SkillNotFoundException;
@@ -6,73 +7,65 @@ import com.apps.PortfolioApp.Model.Entity.Skill;
 import com.apps.PortfolioApp.Repository.SkillRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
 @Service
 public class SkillService {
 
     // step1 ----> Create an object from SkillRepository class :
     private final SkillRepository skillRepository;
 
-
     // step2 ----> Create a con :
     public SkillService(SkillRepository skillRepository) {
         this.skillRepository = skillRepository;
     }
 
-
     // step3 ----> here I will Create a function to Insert to db :
     public SkillDTO insertSkill(SkillDTO skillDTO) {
+        if (skillRepository.findByName(skillDTO.getName()).isPresent()) {
+            throw new DuplicateSkillException("Skill already exists with name:" + skillDTO.getName());
+        }
         Skill skill = SkillDTO.fromDTOToEntity(skillDTO);
         return SkillDTO.fromEntityToDTO(skillRepository.save(skill));
     }
-
 
     // step4 ----> here I will Create a function to get all Skills from db :
     public List<SkillDTO> getAllSkills() {
         if (skillRepository.findAll().isEmpty()) {
             throw new SkillNotFoundException("You don't have any skill");
-        }
-        else {
+        } else {
             List<Skill> skills = skillRepository.findAll();
             return skills.stream().map(SkillDTO::fromEntityToDTO).toList();
         }
     }
 
-
     // step5 ----> here I will Create a function to get a skill by id :
     public SkillDTO getSkillByID(int id) {
         Skill skill = skillRepository.findById(id).orElseThrow(
-                () -> new SkillNotFoundException("Skill not found with id: " + id)
-        );
+                () -> new SkillNotFoundException("Skill not found with id: " + id));
         return SkillDTO.fromEntityToDTO(skill);
     }
 
-
     // step6 ----> here I will Create a function update a skill from db :
-    public SkillDTO updateSkill(String name,SkillDTO skillDTO) {
+    public SkillDTO updateSkill(String name, SkillDTO skillDTO) {
         Skill skill = skillRepository.findByName(name).orElseThrow(
-                () -> new SkillNotFoundException("Skill not found with name: " + name)
-        );
+                () -> new SkillNotFoundException("Skill not found with name: " + name));
         skill.setName(skillDTO.getName());
         skill.setLevel(skillDTO.getLevel());
         skill.setIcon(skillDTO.getIcon());
         return SkillDTO.fromEntityToDTO(skillRepository.save(skill));
     }
 
-
     // step7 ----> here I will Create a function delete a skill from db :
     public void deleteSkill(int id) {
         Skill skill = skillRepository.findById(id).orElseThrow(
-                () -> new SkillNotFoundException("Skill not found with id: " + id)
-        );
+                () -> new SkillNotFoundException("Skill not found with id: " + id));
         skillRepository.delete(skill);
     }
-
 
     // step8 ----> here I will Create a function to find by Skill name :
     public SkillDTO getByName(String name) {
         Skill skill = skillRepository.findByName(name).orElseThrow(
-                () -> new SkillNotFoundException("Skill not found with skill name: " + name)
-        );
+                () -> new SkillNotFoundException("Skill not found with skill name: " + name));
         return SkillDTO.fromEntityToDTO(skill);
     }
 

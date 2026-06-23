7a3f7d7df26758e2cf029d550ffac00a64823000
Mansour <mansour.06.ab.06@gmail.com>
2026-06-23 06:23:16 +0300

    Building a projects page with some modifications

--- lib/widgets/project_card.dart
+++ lib/widgets/project_card.dart
@@ -1,1 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:protfolio_app/Models/project.dart';
+import 'package:protfolio_app/utils/app_colors.dart';
+import 'package:protfolio_app/widgets/custom_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+class ProjectCard extends StatelessWidget {
+  //step1 ----> Create class variables :
+  final Project project;
+
+  //step2 ----> Create constructor :
+  const ProjectCard({super.key, required this.project});
+
+  //step3 ----> here I will call build function to create the widegt :
+  @override
+  Widget build(BuildContext context) {
+    final technologies = project.techStack.split(",");
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              project.name,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textPrimary,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              project.description,
+              style: TextStyle(
+                color: AppColors.textSecondary,
+                height: 1.5,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: technologies.map((tech) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.withValues(alpha: 0.15),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    tech.trim(),
+                    style: TextStyle(
+                      color: AppColors.textPrimary,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+
+            SizedBox(height: 20),
+            CustomButton(
+              tilte: "GitHub",
+
+              width: double.infinity,
+              icon: Icons.code,
+              onPressed: () async {
+                final uri = Uri.parse(project.githubLink);
+                print(project.githubLink);
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri, mode: LaunchMode.platformDefault);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
